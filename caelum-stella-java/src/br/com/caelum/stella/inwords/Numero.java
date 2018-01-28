8370f06a8df44d0ee9831d2cb4773034499cd02e
rodrigo vieira pinto <rodrigovieirapinto@gmail.com>
2018-01-28 13:58:03 -0200

    Tornei a classe Numero utilizável para a atividade 03 da aula 4, removendo refatoração realizada.

--- caelum-stella-java/src/br/com/caelum/stella/inwords/Numero.java
+++ caelum-stella-java/src/br/com/caelum/stella/inwords/Numero.java
@@ -29,12 +29,13 @@ public class Numero {
         double posicao = 1;
         GrupoDe3Digitos grupo = null;
 
-        do {
+        while(true) {
             grupo = new GrupoDe3Digitos((long)(numero % (double)1000), posicao, grupo);
             posicao++;
             numero /= 1000;
+            
+            if(numero <= 0) break;
         }
-        while (numero > 0);
 
         return grupo;
     }
