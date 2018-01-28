72dab8d5f8cd7c10370a9563e899fe894737f80e
rodrigo vieira pinto <rodrigovieirapinto@gmail.com>
2018-01-28 16:12:31 -0200

    Refaroração simples de código.

--- refatoracao/src/aula06/r12/extractclass/depois/Empresa.java
+++ refatoracao/src/aula06/r12/extractclass/depois/Empresa.java
@@ -33,13 +33,15 @@ public class Empresa {
 	public void UpdateEnderecoEntrega(String endEntregaLogradouro, String endEntregaNumero,
 			String endEntregaComplemento, String endEntregaBairro, String endEntregaCEP, String endEntregaMunicipio,
 			String endEntregaUF) {
-		endEntrega.update(endEntregaLogradouro, endEntregaNumero, endEntregaComplemento, endEntregaBairro, endEntregaCEP, endEntregaMunicipio, endEntregaUF);
+		endEntrega.update(endEntregaLogradouro, endEntregaNumero, endEntregaComplemento, endEntregaBairro,
+				endEntregaCEP, endEntregaMunicipio, endEntregaUF);
 	}
 
 	public void UpdateEnderecoComercial(String endComercialLogradouro, String endComercialNumero,
 			String endComercialComplemento, String endComercialBairro, String endComercialCEP,
 			String endComercialMunicipio, String endComercialUF) {
-		endComercial.update(endComercialLogradouro, endComercialNumero, endComercialComplemento, endComercialBairro, endComercialCEP, endComercialMunicipio, endComercialCEP);
+		endComercial.update(endComercialLogradouro, endComercialNumero, endComercialComplemento, endComercialBairro,
+				endComercialCEP, endComercialMunicipio, endComercialCEP);
 	}
 }
 
@@ -53,19 +55,13 @@ class Endereco {
 	private String municipio;
 	private String uf;
 
-	public Endereco(String endEntregaLogradouro, String endEntregaNumero, String endEntregaComplemento,
-			String endEntregaBairro, String endEntregaCEP, String endEntregaMunicipio, String endEntregaUF) {
-		this.logradouro = endEntregaLogradouro;
-		this.numero = endEntregaNumero;
-		this.complemento = endEntregaComplemento;
-		this.bairro = endEntregaBairro;
-		this.cep = endEntregaCEP;
-		this.municipio = endEntregaMunicipio;
-		this.uf = endEntregaUF;
+	public Endereco(String logradouro, String numero, String complemento, String bairro, String cep, String municipio,
+			String uf) {
+		update(logradouro, numero, complemento, bairro, cep, municipio, uf);
 	}
 
-	public void update(String logradouro, String numero, String complemento,
-			String bairro, String cep, String municipio, String uf) {
+	public void update(String logradouro, String numero, String complemento, String bairro, String cep,
+			String municipio, String uf) {
 		this.logradouro = logradouro;
 		this.numero = numero;
 		this.complemento = complemento;
