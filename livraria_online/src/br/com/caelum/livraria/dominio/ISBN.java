87a6f2749aaf3044bced7702874ba724289985fb
rodrigovieirapinto <rodrigovieirapinto@192.168.0.19>
2018-07-09 10:41:53 -0300

    8 (substituir método por objeto método):
     * código que deve ser refatorado usando essa técnica;

--- livraria_online/src/br/com/caelum/livraria/dominio/ISBN.java
+++ livraria_online/src/br/com/caelum/livraria/dominio/ISBN.java
@@ -10,27 +10,25 @@ public class ISBN {
 		this.numero = isbn;
 	}
 
-	// 8 - substituir método por objeto método: aqui pode ser aplicada essa técnica de refatoração 
+	// 8 - substituir método por objeto método: aplicar essa técnica de refatoração 
 	// para criar um validador de ISBN.
 	private boolean validar(String isbn) {
 		if (isbn == null) return false;
-		
-		// 7 - remover atribuições a parâmetros: é possível de usar esse código para exemplificar?
-		isbn = isbn.replaceAll("-", "");
-		if(isbn.replaceAll( "-", "" ).length() != 13) return false;
+
+		String isbnSomenteNumeros = isbn.replaceAll("-", "");
+		if(isbnSomenteNumeros.length() != 13) return false;
 		
 		try{
 			int tot = 0;
 			for (int i = 0; i < 12; i++ ){
-				int digito = Integer.parseInt(isbn.substring(i, i + 1));
+				int digito = Integer.parseInt(isbnSomenteNumeros.substring(i, i + 1));
 				tot += (i % 2 == 0) ? digito * 1 : digito * 3;
 			}
 
 			int checksum = 10 - (tot % 10);
-			if ( checksum == 10 ) checksum = 0;
-			
+			if (checksum == 10) checksum = 0;
 
-			return checksum == Integer.parseInt(isbn.substring(12));
+			return checksum == Integer.parseInt(isbnSomenteNumeros.substring(12));
 		}
 		catch (NumberFormatException nfe){
 			return false;
