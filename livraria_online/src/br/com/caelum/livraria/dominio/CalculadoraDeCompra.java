84200106b6f0d8bfef079a4b8088fcd5808cc5cc
rodrigovieirapinto <rodrigovieirapinto@192.168.0.19>
2018-07-09 10:23:39 -0300

    7 (remover atribuições a parâmetros):
     * código bagunçado para aplicação dessa refatoração;

--- livraria_online/src/br/com/caelum/livraria/dominio/CalculadoraDeCompra.java
+++ livraria_online/src/br/com/caelum/livraria/dominio/CalculadoraDeCompra.java
@@ -4,12 +4,11 @@ import org.javamoney.moneta.Money;
 
 public class CalculadoraDeCompra {
 	
-	// 7 - remover atribuições a parametros: preparar código para usar essa refatoração.
+	// 7 - remover atribuições a parametros: aplicar essa técnica de refatoração.
 	public Money calcularValorTotal(Money subtotal, Money valorDoFrete, Desconto desconto) {
-		Money valorTotal = subtotal;
-		if(valorTotal.isZero()) return valorTotal;
-		valorTotal = valorTotal.subtract(desconto.getValor());
-		valorTotal = valorTotal.add(valorDoFrete);
-		return valorTotal;
+		if(subtotal.isZero()) return subtotal;
+		subtotal = subtotal.subtract(desconto.getValor());
+		subtotal = subtotal.add(valorDoFrete);
+		return subtotal;
 	}
 }
